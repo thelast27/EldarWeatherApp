b4b0cd28246fcd580b15eb05c297a1b39b8a4ea7
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-26 12:40:06 +0300

    add code for main daily table view cell

--- EldarWeatherApp/Cells/MainTableViewCells/MainDailyTableViewSection/MainDailyTableViewCell.swift
+++ EldarWeatherApp/Cells/MainTableViewCells/MainDailyTableViewSection/MainDailyTableViewCell.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class MainDailyTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var dailyWeather: [DailyWeatherData]?
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: DailyTableViewCell.key, bundle: nil), forCellReuseIdentifier: DailyTableViewCell.key)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +24,29 @@ class MainDailyTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func update(with date: [DailyWeatherData]) {
+        self.dailyWeather = date
+        self.tableView.reloadData()
+    }
+
+    
+}
+
+extension MainDailyTableViewCell: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dailyWeather?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: DailyTableViewCell.key, for: indexPath) as? DailyTableViewCell else { return UITableViewCell() }
+        if let daily = dailyWeather {
+            cell.update(date: daily[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
     
 }
