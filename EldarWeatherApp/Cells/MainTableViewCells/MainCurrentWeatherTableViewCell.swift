a9d1cb1e7d593ad620e8cfa6868249b21756716e
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-26 15:37:12 +0300

    add outlets for cell and send data from server

--- EldarWeatherApp/Cells/MainTableViewCells/MainCurrentWeatherTableViewCell.swift
+++ EldarWeatherApp/Cells/MainTableViewCells/MainCurrentWeatherTableViewCell.swift
@@ -8,16 +8,43 @@
 import UIKit
 
 class MainCurrentWeatherTableViewCell: UITableViewCell {
-
+    
+    
+    
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var weatherDiscrip: UILabel!
+    @IBOutlet weak var minTemp: UILabel!
+    @IBOutlet weak var maxTemp: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
+    func update(date: CurrentAndForecastWeather) {
+        DispatchQueue.main.async {
+            guard let temp = date.current?.temp,
+                  let cityName = date.timeZone,
+                  let description = date.current?.weather?.first?.description,
+                  let todayMin = date.daily?.first?.temp?.min,
+                  let todayMax = date.daily?.first?.temp?.max
+            else { return }
+            
+            self.currentTemp.text = "\(Int(temp)) °"
+            self.cityName.text = cityName
+            self.weatherDiscrip.text = "Now \(description)"
+            self.minTemp.text = "Min.:\(Int(todayMin))°,"
+            self.maxTemp.text = "Max.:\(Int(todayMax))°"
+        }
+        
+        
+        
+    }
 }
