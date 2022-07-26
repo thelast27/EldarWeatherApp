a95d4b31128704f004f657ac32f9a9f694bc03ca
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-26 15:28:52 +0300

    add cell for current weather

---
+++ EldarWeatherApp/Cells/MainTableViewCells/MainCurrentWeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainCurrentWeatherTableViewCell.swift
+//  EldarWeatherApp
+//
+//  Created by Eldar Garbuzov on 26.07.22.
+//
+
+import UIKit
+
+class MainCurrentWeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
