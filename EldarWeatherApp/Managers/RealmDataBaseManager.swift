5b33bf1c0de72db3075599a9ffed2599a9ae1270
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-29 15:25:53 +0300

    add files for model and manager realm

---
+++ EldarWeatherApp/Managers/RealmDataBaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  RealmDataBaseManager.swift
+//  EldarWeatherApp
+//
+//  Created by Eldar Garbuzov on 29.07.22.
+//
+
+import Foundation
---
+++ EldarWeatherApp/Models/RealmWeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  RealmWeatherModel.swift
+//  EldarWeatherApp
+//
+//  Created by Eldar Garbuzov on 29.07.22.
+//
+
+import Foundation
