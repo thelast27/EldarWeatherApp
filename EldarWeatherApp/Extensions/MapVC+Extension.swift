74156f43676addbae1a1545877a748eca5f8d007
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-29 14:53:45 +0300

    add extension for map VC

---
+++ EldarWeatherApp/Extensions/MapVC+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  MapVC+Extension.swift
+//  EldarWeatherApp
+//
+//  Created by Eldar Garbuzov on 29.07.22.
+//
+
+import Foundation
+import GoogleMaps
+
+extension MapViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        weatherManagerDelegte.getWeatherForCityCoordinates(long: coordinate.longitude, lat: coordinate.latitude, withLang: .english, withUnitsOfmeasurement: .celsius) { weatherData in
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                guard let temp = weatherData.current?.temp,
+                      let cityName = weatherData.timeZone
+                else { return }
+                self.nameOfPoint.isHidden = false
+                self.currentTemp.isHidden = false
+                self.currentTemp.text = "\(Int(temp)) °"
+                self.nameOfPoint.text = cityName
+            }
+        }
+    }
+}
