2476fbaa61b0d6061bfca2604c68859241cfc7ab
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-29 11:49:19 +0300

    add new point for tabbar

--- EldarWeatherApp/ViewControllers/TabBarViewController.swift
+++ EldarWeatherApp/ViewControllers/TabBarViewController.swift
@@ -12,10 +12,10 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let weatherViewContoller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainViewController") as? ViewController else {return}
+        guard let weatherViewContoller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainViewController") as? ViewController, let mapVC = UIStoryboard(name: "MapViewController", bundle: nil).instantiateViewController(withIdentifier: "MapViewController") as? MapViewController else {return}
 
         
-        viewControllers = [addViewController(viewController: UINavigationController(rootViewController: weatherViewContoller), title: "Weather", image: UIImage(systemName: "cloud.sun.bolt"))]
+        viewControllers = [addViewController(viewController: UINavigationController(rootViewController: weatherViewContoller), title: "Weather", image: UIImage(systemName: "cloud.sun.bolt")), addViewController(viewController: UINavigationController(rootViewController: mapVC), title: "Map", image: UIImage(systemName: "map"))]
         
         
     }
