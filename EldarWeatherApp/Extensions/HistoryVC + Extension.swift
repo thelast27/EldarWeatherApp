ac4835b189a62d8ff7027da4d92d7b00952ee152
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-29 15:37:39 +0300

    add delegate and data source

--- EldarWeatherApp/Extensions/HistoryVC + Extension.swift
+++ EldarWeatherApp/Extensions/HistoryVC + Extension.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import RealmSwift
+import Realm
+
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        resultsRealmData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = historyTableView.dequeueReusableCell(withIdentifier: "HistoryTableViewCell", for: indexPath) as? HistoryTableViewCell else { return UITableViewCell() }
+        cell.configuration(data: resultsRealmData[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        historyTableView.deselectRow(at: indexPath, animated: true)
+    }
+}
