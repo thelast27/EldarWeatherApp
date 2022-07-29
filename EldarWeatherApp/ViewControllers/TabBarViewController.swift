80c6ee2ea06f842c8440251fa3bddac0b5865ba5
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-29 15:06:31 +0300

    add new point for tabbar

--- EldarWeatherApp/ViewControllers/TabBarViewController.swift
+++ EldarWeatherApp/ViewControllers/TabBarViewController.swift
@@ -12,10 +12,10 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let weatherViewContoller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainViewController") as? ViewController, let mapVC = UIStoryboard(name: "MapViewController", bundle: nil).instantiateViewController(withIdentifier: "MapViewController") as? MapViewController else {return}
+        guard let weatherViewContoller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainViewController") as? ViewController, let mapVC = UIStoryboard(name: "MapViewController", bundle: nil).instantiateViewController(withIdentifier: "MapViewController") as? MapViewController, let historyVC = UIStoryboard(name: "HistoryViewController", bundle: nil).instantiateViewController(withIdentifier: "HistoryViewController") as? HistoryViewController else {return}
 
         
-        viewControllers = [addViewController(viewController: UINavigationController(rootViewController: weatherViewContoller), title: "Weather", image: UIImage(systemName: "cloud.sun.bolt")), addViewController(viewController: UINavigationController(rootViewController: mapVC), title: "Map", image: UIImage(systemName: "map"))]
+        viewControllers = [addViewController(viewController: UINavigationController(rootViewController: weatherViewContoller), title: "Weather", image: UIImage(systemName: "cloud.sun.bolt")), addViewController(viewController: UINavigationController(rootViewController: mapVC), title: "Map", image: UIImage(systemName: "map")), addViewController(viewController: UINavigationController(rootViewController: historyVC), title: "History", image: UIImage(systemName: "book"))]
         
         
     }
