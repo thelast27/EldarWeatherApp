699831a9966461ecec3c3d455d87e9ffc31c6e76
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-07-29 15:22:38 +0300

    add cells for historyVC

---
+++ EldarWeatherApp/Cells/HistoryTableViewCells/HistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryTableViewCell.swift
+//  EldarWeatherApp
+//
+//  Created by Eldar Garbuzov on 29.07.22.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
